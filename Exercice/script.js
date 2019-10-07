11ac1f3f00e7b75cff69dcc2909451403dca7150
cpnvbenoit <47849605+cpnvbenoit@users.noreply.github.com>
2019-10-07 15:57:59 +0200

    exo 6 funi exo 7 wip 75%

--- Exercice/script.js
+++ Exercice/script.js
@@ -1,50 +1,85 @@
-let bleu=document.getElementById('cmdbleutrace');
-let bleureti=document.getElementById('divreti');
-let hover=document.getElementById('hvrarial');
-let yes1=document.getElementById('cbxyes1');
-let yes2=document.getElementById('cbxyes2');
-let killyes=document.getElementById('cbxyes');
-let disayes=document.getElementById('greenyes');
-let clkdestr=document.getElementById('destrdiv');
-let divacahe=document.getElementById('divacacher');
-let changtext=document.getElementById('textName');
-let initials=document.getElementById('txtInitials');
-bleu.addEventListener('click',funbleu);
-bleureti.addEventListener('click',funbleureti);
-hover.addEventListener('mouseover',funhover);
-hover.addEventListener('mouseout',funpashover);
-clkdestr.addEventListener('click',fundestrdiv);
-yes1.addEventListener('click',funyes);
-yes2.addEventListener('click',funyes);
-changtext.addEventListener('change',funnom)
+let bleu = document.getElementById('cmdbleutrace');
+let bleureti = document.getElementById('divreti');
+let hover = document.getElementById('hvrarial');
+let yes1 = document.getElementById('cbxyes1');
+let yes2 = document.getElementById('cbxyes2');
+let killyes = document.getElementById('cbxyes');
+let disayes = document.getElementById('greenyes');
+let clkdestr = document.getElementById('destrdiv');
+let divacahe = document.getElementById('divacacher');
+let Pomme = document.getElementById("txtInitials");
+let txtchange = document.getElementById('txtName');
+bleu.addEventListener('click', funbleu);
+bleureti.addEventListener('click', funbleureti);
+hover.addEventListener('mouseover', funhover);
+hover.addEventListener('mouseout', funpashover);
+clkdestr.addEventListener('click', fundestrdiv);
+yes1.addEventListener('click', funyes);
+yes2.addEventListener('click', funyes);
+txtchange.addEventListener('keyup', funnom);
+avm.addEventListener('click', funavm);
+pre.addEventListener('click', funpre);
+par.style.display="none";
 function funbleu() {
     bleu.classList.add('cssbleutrace')
 }
+
 function funbleureti() {
     bleu.classList.remove('cssbleutrace')
 }
+
 function funhover() {
     hover.classList.add('csshover')
 }
+
 function funpashover() {
     hover.classList.remove('csshover')
 }
+
 function funyes() {
-    if ((yes1.checked === true)&&(yes2.checked === true)){
+    if ((yes1.checked === true) && (yes2.checked === true)) {
         killyes.style.display = "none";
         disayes.style.display = "block";
     }
 }
+
 function fundestrdiv() {
     divacahe.style.display = "none"
 }
 function funnom() {
-    let espace=" ";
-    let valeurespace=changtext.indexOf(espace);
-    let longeur=changtext.length;
-    let carac1=changtext[1];
-    let carac2=changtext[valeurespace+1];
-    let carac3=changtext[longeur];
-    initials=carac1+carac2+carac3;
+    bleu.classList.add("test");
+    let espace = " ";
+    console.log('le text a changer');
+    var valeurespace = txtchange.value.lastIndexOf(espace);
+    var longeur = txtchange.value.length;
+    var carac1 = txtchange.value.charAt(0);
+    var carac2 = txtchange.value.charAt(valeurespace + 1);
+    var carac3 = txtchange.value.charAt(longeur - 1);
+    initials = carac1 + carac2 + carac3;
+    console.log(initials);
+    Pomme.value = initials.toUpperCase()
+}
 
+function funavm() {
+    console.log('etape 1');
+    document.getElementById('avm').disabled = true;
+    document.getElementById('pre').disabled = false;
+    document.getElementById('par').disabled = true;
+    par.style.display="block";
+    par.style.display="block";
+    par.style.display="block";
+    par.style.display="block";
+    pre.style.disabled = true;
+    par.style.disabled = false;
+}
+
+function funpre() {
+    avm.style.display="none";
+    document.getElementById('pre').disabled = true;
+    document.getElementById('par').disabled = false;
+}
+
+function funpar() {
+    avm.style.display="block";
+    .style.display="block";
 }
