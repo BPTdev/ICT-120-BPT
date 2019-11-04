82ed52165fb55ea6787a41f8a87d8cd586667963
cpnvbenoit <47849605+cpnvbenoit@users.noreply.github.com>
2019-11-04 16:56:41 +0100

    projet wip

--- Projet/test/test.js
+++ Projet/test/test.js
@@ -1,15 +1,3 @@
 document.addEventListener('DOMContentLoaded',init);
 window.onscroll = function() {myFunction()};
-;
-var sticky = navbar.offsetTop;
 
-function init() {
-  myFunction;
-}
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
